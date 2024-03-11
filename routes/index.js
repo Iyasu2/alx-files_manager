58a2cbf19d9505451b4b6f7244b6ea0955b98eef
iyasu2 <iyasuforreal@gmail.com>
2024-03-11 20:08:36 +0300

    start

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,12 @@ const router = express.Router();
 // Import the AppController methods
 const AppController = require('../controllers/AppController');
 
+// Import the UsersController methods
+const UsersController = require('../controllers/UsersController');
+
 // Define endpoints
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
